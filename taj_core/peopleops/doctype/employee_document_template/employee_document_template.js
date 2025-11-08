952c6f97668052b9c2434fe38f7a442fb53a775c
IT-Tajff <it@tajff.com>
2025-11-08 19:13:54 +0300

    UPdate Employee Letter

--- taj_core/peopleops/doctype/employee_document_template/employee_document_template.js
+++ taj_core/peopleops/doctype/employee_document_template/employee_document_template.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2025, Maged Bajandooh and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Employee Letter Template", {
+// frappe.ui.form.on("Employee Document Template", {
 // 	refresh(frm) {
 
 // 	},
