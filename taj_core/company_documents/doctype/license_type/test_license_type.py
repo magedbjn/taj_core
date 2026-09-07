fc310ac92bf9bfb1f5d73939addf5ee303b55c5e
IT-Tajff <it@tajff.com>
2026-09-07 11:09:08 +0300

    test: remove empty doctype test skeletons

--- taj_core/company_documents/doctype/license_type/test_license_type.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, m.bajandooh@tajff.sa and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestLicenseType(FrappeTestCase):
-	pass
